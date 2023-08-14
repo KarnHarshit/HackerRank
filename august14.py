2bf72800a9893d03f45f52504baff2718fc8e040
KarnHarshit <harshitkarn23@gmail.com>
2023-08-14 20:12:01 +0530

    SplitScreen

---
+++ august14.py
@@ -0,0 +1,11 @@
+# input
+input_string = input()
+
+# Split the string on space delimiter
+split_parts = input_string.split(" ")
+
+# Join the split parts using hyphen
+result = "-".join(split_parts)
+
+# Print the result
+print(result)
